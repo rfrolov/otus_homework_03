d6dc6a0a9e30bc1928c1d23d7ab2c1212c60361c
rfrolov <rfrolov@engineer.com>
2018-08-09 22:25:09 +0300

    1. Исправлена ошибка в процедуре удаления элемента через кастомный аллокатор. 2. Добавлены тесты. 3. В аллокаторе добавлена проверка что N >= 1;

--- inc/allocator.h
+++ inc/allocator.h
@@ -3,68 +3,71 @@
 #include <bitset>
 
 namespace custom {
-template<typename T, size_t N = 1>
-struct allocator {
-    using value_type = T;
+    template<typename T, size_t N = 1>
+    struct allocator {
+        using value_type = T;
 
-    allocator() = default;
-    ~allocator() = default;
+        allocator() = default;
 
-    template<typename U>
-    struct rebind {
-        using other = allocator<U, N>;
-    };
+        ~allocator() = default;
 
-    T *allocate(std::size_t n) {
-        auto p = addNote(n);
-        if (p == nullptr) {
-            throw std::bad_alloc(); //"Error allocating memory."
+        template<typename U>
+        struct rebind {
+            using other = allocator<U, N>;
+        };
+
+        T *allocate(std::size_t n) {
+            auto p = addNote(n);
+            if (p == nullptr) {
+                throw std::bad_alloc(); //"Error allocating memory."
+            }
+            return p;
         }
-        return p;
-    }
 
-    void deallocate(T *p, std::size_t n) {
-        if (!delNote(p, n)) {
-            throw std::bad_alloc(); //"Error releasing memory."
-        }
-    }
-
-    template<typename U, typename ...Args>
-    void construct(U *p, Args &&...args) {
-        new(p) U(std::forward<Args>(args)...);
-    }
-
-    void destroy(T *p) {
-        p->~T();
-    }
-
-  private:
-    T *addNote(size_t n) {
-        size_t      cnt = 0;
-        for (size_t i   = 0; i < N; ++i) {
-            cnt = (!mask_.test(i)) ? cnt + 1 : 0;
-            if (cnt == n) {
-                auto firstElement = i + 1 - n;
-                mask_.set(firstElement);
-                return &data_[firstElement];
+        void deallocate(T *p, std::size_t n) {
+            if (!delNote(p, n)) {
+                throw std::bad_alloc(); //"Error releasing memory."
             }
         }
-        return nullptr;
-    }
 
-    bool delNote(T *p, std::size_t n) {
-        if (n > N || p < data_.data()) {
-            return false;
+        template<typename U, typename ...Args>
+        void construct(U *p, Args &&...args) {
+            new(p) U(std::forward<Args>(args)...);
         }
 
-        for (size_t i = p - data_.data(); i < n; ++i) {
-            mask_.reset(static_cast<size_t>(i));
+        void destroy(T *p) {
+            p->~T();
         }
-        return true;
-    }
 
-  private:
-    std::bitset<N>   mask_{0};
-    std::array<T, N> data_;
-};
+    private:
+        T *addNote(size_t n) {
+            size_t cnt = 0;
+            for (size_t i = 0; i < N; ++i) {
+                cnt = (!mask_.test(i)) ? cnt + 1 : 0;
+                if (cnt == n) {
+                    auto firstElement = i + 1 - n;
+                    mask_.set(firstElement);
+                    return &data_[firstElement];
+                }
+            }
+            return nullptr;
+        }
+
+        bool delNote(T *p, std::size_t n) {
+            if (n > N || p < &data_[0] || p > &data_[N - 1]) {
+                return false;
+            }
+
+            for (size_t i = p - data_.data(); i < n; ++i) {
+                mask_.reset(static_cast<size_t>(i));
+            }
+            return true;
+        }
+
+    private:
+        std::bitset<N> mask_{0};
+        std::array<T, N> data_;
+
+        static_assert(N >= 1, "N must be greater than or equal to 1.");
+    };
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -3,6 +3,20 @@
 #include "../inc/factorial.h"
 #include "../inc/allocator.h"
 
+struct test_class {
+    test_class() {  std::cout << "ctor" << std::endl;  ctor_called = true; };
+
+    ~test_class() { dtor_called = true; };
+
+    bool is_ctor_called() { return ctor_called; }
+
+    bool is_dtor_called() { return dtor_called; }
+
+private:
+    bool ctor_called = false;
+    bool dtor_called = false;
+};
+
 TEST(lib, version) {
     EXPECT_GT(version(), 0);
 }
@@ -18,46 +32,146 @@ TEST(factorial, factorial) {
     EXPECT_EQ(factorial(7), 5040);
     EXPECT_EQ(factorial(8), 40320);
     EXPECT_EQ(factorial(9), 362880);
-    EXPECT_EQ(factorial(10),3628800);
-    EXPECT_EQ(factorial(11),39916800);
-    EXPECT_EQ(factorial(12),479001600);
-    EXPECT_EQ(factorial(13),6227020800);
-    EXPECT_EQ(factorial(14),87178291200);
-    EXPECT_EQ(factorial(15),1307674368000);
-    EXPECT_EQ(factorial(16),20922789888000);
-    EXPECT_EQ(factorial(17),355687428096000);
-    EXPECT_EQ(factorial(18),6402373705728000);
-    EXPECT_EQ(factorial(19),121645100408832000);
-    EXPECT_EQ(factorial(20),2432902008176640000);
+    EXPECT_EQ(factorial(10), 3628800);
+    EXPECT_EQ(factorial(11), 39916800);
+    EXPECT_EQ(factorial(12), 479001600);
+    EXPECT_EQ(factorial(13), 6227020800);
+    EXPECT_EQ(factorial(14), 87178291200);
+    EXPECT_EQ(factorial(15), 1307674368000);
+    EXPECT_EQ(factorial(16), 20922789888000);
+    EXPECT_EQ(factorial(17), 355687428096000);
+    EXPECT_EQ(factorial(18), 6402373705728000);
+    EXPECT_EQ(factorial(19), 121645100408832000);
+    EXPECT_EQ(factorial(20), 2432902008176640000);
 }
 
-TEST(allocator, size) {
-    constexpr unsigned int n = 10;
-    using type = int;
+//TEST(allocator, size) {
+//    constexpr unsigned int n = 10;
+//    using type = test_class;
+//
+//    custom::allocator<type, n> allocator;
+//    EXPECT_EQ(sizeof(allocator), sizeof(type) * n + sizeof(std::bitset<n>));
+//}
 
-    custom::allocator<type, n> allocator;
-    EXPECT_EQ(sizeof(allocator), sizeof(type) * n + sizeof(std::bitset<n>));
+//TEST(allocator, allocate_one_element) {
+//    using type = test_class;
+//    constexpr auto n = 1;
+//    using allocator_t = custom::allocator<type, n>;
+//
+//    type *element = nullptr;
+//    try {
+//        allocator_t allocator;
+//        element = allocator.allocate(n);
+//    }
+//    catch (const std::bad_alloc &e) {
+//        FAIL() << e.what();
+//    }
+//
+//    EXPECT_NE(element, nullptr);
+//}
+//
+//TEST(allocator, allocate_more_elements) {
+//    using type = test_class;
+//    constexpr auto n = 1;
+//    using allocator_t = custom::allocator<type, n>;
+//
+//    auto is_alloc_fail = false;
+//    try {
+//        allocator_t allocator;
+//        allocator.allocate(n + 1);
+//    }
+//    catch (const std::bad_alloc &e) {
+//        is_alloc_fail = true;
+//    }
+//
+//    EXPECT_TRUE(is_alloc_fail);
+//}
+//
+//TEST(allocator, deallocate_true_element) {
+//    using type = test_class;
+//    constexpr auto n = 1;
+//    using allocator_t = custom::allocator<type, n>;
+//
+//    try {
+//        allocator_t allocator;
+//        auto element = allocator.allocate(n);
+//        allocator.deallocate(element, n);
+//    }
+//    catch (const std::bad_alloc &e) {
+//        FAIL() << e.what();
+//    }
+//}
+//
+//TEST(allocator, deallocate_fake_element) {
+//    using type = test_class;
+//    constexpr auto n = 1;
+//    using allocator_t = custom::allocator<type, n>;
+//
+//    auto is_deallocate_fail = false;
+//    try {
+//        allocator_t allocator;
+//        auto element = allocator.allocate(n);
+//        allocator.deallocate(element + 1, n);
+//    }
+//    catch (const std::bad_alloc &e) {
+//        is_deallocate_fail = true;
+//    }
+//
+//    EXPECT_TRUE(is_deallocate_fail);
+//}
+//
+//TEST(allocator, deallocate_fake_num) {
+//    using type = test_class;
+//    constexpr auto n = 1;
+//    using allocator_t = custom::allocator<type, n>;
+//
+//    auto is_deallocate_fail = false;
+//    try {
+//        allocator_t allocator;
+//        auto element = allocator.allocate(n);
+//        allocator.deallocate(element, n + 1);
+//    }
+//    catch (const std::bad_alloc &e) {
+//        is_deallocate_fail = true;
+//    }
+//
+//    EXPECT_TRUE(is_deallocate_fail);
+//}
+//
+//TEST(allocator, construct) {
+//    using type = test_class;
+//    constexpr auto n = 1;
+//    using allocator_t = custom::allocator<type, n>;
+//
+//    try {
+//        allocator_t allocator;
+//        auto element = allocator.allocate(n);
+//        allocator.construct(element);
+//        EXPECT_TRUE(element->is_ctor_called());
+//    }
+//    catch (const std::bad_alloc &e) {
+//        FAIL() << e.what();
+//    }
+//}
+
+TEST(allocator, destruct) {
+    using type = test_class;
+    constexpr auto n = 5;
+    constexpr auto value = 123;
+    using allocator_t = custom::allocator<type, n>;
+
+    try {
+        auto *allocator = new allocator_t;
+        auto element = allocator->allocate(n);
+        // TODO: двойной вызов конструктора.
+        allocator->construct(element);
+        allocator->destroy(element);
+    }
+    catch (const std::bad_alloc &e) {
+        FAIL() << e.what();
+    }
 }
 
-TEST(allocator, allocate_one_element) {
-    // TODO: Добавить проверку.
-    EXPECT_EQ(1,0);
-}
-
-TEST(allocator, allocate_more_elements) {
-    // TODO: Добавить проверку.
-    EXPECT_EQ(1,0);
-}
-
-TEST(allocator, deallocate_true_element) {
-    // TODO: Добавить проверку.
-    EXPECT_EQ(1,0);
-}
-
-TEST(allocator, deallocate_fake_element) {
-    // TODO: Добавить проверку.
-    EXPECT_EQ(1,0);
-}
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
